9342ffd2feee3050a2134df3ef517eb75622ba66
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-17 17:39:26 +0530

    Refactored store to separate module

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import userReducer from './reducers/userReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import productReducer from './reducers/productReducer';
-import cartReducer from './reducers/cartReducer';
-
-const reducer = combineReducers({
-  products: productReducer,
-  user: userReducer,
-  cart: cartReducer,
-});
-
-const store = createStore(reducer, composeWithDevTools());
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
