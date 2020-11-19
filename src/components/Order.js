6b2797dad31a174bb99df36027bddbbb496495c8
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-19 19:34:37 +0530

    Added Order component to display separate orders

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import '../css/Order.css';
+import moment from 'moment';
+import CartProduct from './CartProduct';
+import CurrencyFormat from 'react-currency-format';
 
-function Order() {
-  return <div></div>;
+function Order({ order }) {
+  return (
+    <div className="order">
+      <h2>Order</h2>
+      <p>{order.createdAt}</p>
+      <p className="order__id">
+        <small>{order._id}</small>
+      </p>
+      {order.orderItems.map((item) => (
+        <CartProduct
+          hideButtons={true}
+          key={item.productId._id}
+          price={item.productId.price}
+          id={item.productId._id}
+          title={item.productId.title}
+          image={`http://${item.productId.images[0]}`}
+          rating={5}
+        ></CartProduct>
+      ))}
+      <CurrencyFormat
+        thousandSpacing={'2s'}
+        decimalScale={2}
+        value={order.totalPrice}
+        displayType={'text'}
+        thousandSeparator={true}
+        renderText={(value) => (
+          <h3 className="order__total">Order Total: ₹{value}</h3>
+        )}
+      ></CurrencyFormat>
+    </div>
+  );
 }
 
 export default Order;
