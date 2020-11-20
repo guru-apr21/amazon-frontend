f36677d97027fe778a7c1e733aaf779a9b79ad65
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-20 14:59:43 +0530

    Added get user products service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,7 +1,28 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api';
+const baseUrl = 'http://localhost:3001/api/products';
+
+let token;
+const getToken = () => {
+  let user = window.localStorage.getItem('loggedInUser');
+  if (user) {
+    user = JSON.parse(user);
+    return user.accessToken;
+  }
+};
 
 export const getAllProducts = async () => {
-  const { data } = await axios.get(`${baseUrl}/products`);
+  const { data } = await axios.get(baseUrl);
   return data;
+};
+
+export const getUserProducts = async (userId) => {
+  try {
+    token = getToken();
+    const { data } = await axios.get(`${baseUrl}/${userId}`, {
+      headers: { 'x-access-token': token },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
