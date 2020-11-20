151ae6bb82a8e72bc03b3f153143d7d5b9de5447
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-20 15:55:17 +0530

    dispatched setuserproducts

--- src/components/ProductUpload.js
+++ src/components/ProductUpload.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserProducts } from '../reducers/productReducer';
 
 function ProductUpload() {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(setUserProducts()), [dispatch]);
+  const products = useSelector((state) => state.products.userProducts);
+
   return (
     <div className="orders">
       <h1>Your Products</h1>
