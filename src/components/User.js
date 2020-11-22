4d56a45c83b45902fe73bd5e05591180fd5af6bc
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-22 23:19:17 +0530

    Defined start selling button

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import '../css/user.css';
-import { clearUser } from '../reducers/userReducer';
+import { clearUser, createSeller, setUser } from '../reducers/userReducer';
 import { clearOrders } from '../reducers/orderReducer';
 import { clearCart } from '../reducers/cartReducer';
 import { useHistory } from 'react-router-dom';
+import '../css/user.css';
 
 function User() {
-  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
   const history = useHistory();
   const handleSignOut = () => {
     dispatch(clearCart());
@@ -16,36 +17,49 @@ function User() {
     dispatch(clearOrders());
     history.replace('/');
   };
-  const handleSeller = () => {};
+
+  const handleSeller = () => {
+    try {
+      dispatch(createSeller());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="user">
-      <h1>Login & Security</h1>
+      {user && (
+        <>
+          <h1>Login & Security</h1>
 
-      <div className="user__details">
-        <ul className="user__lists">
-          <li className="user__list">
-            <span className="user__optionLine1">First Name:</span>
-            <span className="user__optionLine2">{user.firstName}</span>
-          </li>
-          <li className="user__list">
-            <span className="user__optionLine1">Last Name</span>
-            <span className="user__optionLine2">{user.lastName}</span>
-          </li>
-          <li className="user__list">
-            <span className="user__optionLine1">Email</span>
-            <span className="user__optionLine2">{user.email}</span>
-          </li>
-          {user.role === 'seller' && (
-            <li className="user__list">
-              <span className="user__optionLine1">Role</span>
-              <span className="user__optionLine2">{user.role}</span>
-            </li>
+          <div className="user__details">
+            <ul className="user__lists">
+              <li className="user__list">
+                <span className="user__optionLine1">First Name:</span>
+                <span className="user__optionLine2">{user.firstName}</span>
+              </li>
+              <li className="user__list">
+                <span className="user__optionLine1">Last Name</span>
+                <span className="user__optionLine2">{user.lastName}</span>
+              </li>
+              <li className="user__list">
+                <span className="user__optionLine1">Email</span>
+                <span className="user__optionLine2">{user.email}</span>
+              </li>
+              {user.role === 'seller' && (
+                <li className="user__list">
+                  <span className="user__optionLine1">Role</span>
+                  <span className="user__optionLine2">{user.role}</span>
+                </li>
+              )}
+            </ul>
+          </div>
+          <button onClick={handleSignOut}>Sign Out</button>
+          {user.role !== 'seller' && (
+            <button onClick={handleSeller}>Start Selling</button>
           )}
-        </ul>
-      </div>
-      <button onClick={handleSignOut}>Sign Out</button>
-      <button onClick={handleSeller}>Start Selling</button>
+        </>
+      )}
     </div>
   );
 }
