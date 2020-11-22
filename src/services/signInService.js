37aea1c6c7b66af5f8d355bc77d71af7a70e3543
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-22 23:15:52 +0530

    Defined change Role service

--- src/services/signInService.js
+++ src/services/signInService.js
@@ -1,12 +1,34 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api';
+const baseUrl = 'http://localhost:3001/api/users';
+
+const getToken = () => {
+  let user = window.localStorage.getItem('loggedInUser');
+  if (user) {
+    user = JSON.parse(user);
+    console.log(user.accessToken);
+    return user.accessToken;
+  }
+};
 
 export const signin = async (body) => {
-  const { data } = await axios.post(`${baseUrl}/users/signin`, body);
+  const { data } = await axios.post(`${baseUrl}/signin`, body);
   return data;
 };
 
 export const signup = async (body) => {
-  const { data } = await axios.post(`${baseUrl}/users/signup`, body);
+  const { data } = await axios.post(`${baseUrl}/signup`, body);
   return data;
+};
+
+export const changeRole = async () => {
+  try {
+    const token = getToken();
+    console.log(token);
+    const { data } = await axios.put(`${baseUrl}/role`, null, {
+      headers: { 'x-access-token': token },
+    });
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
