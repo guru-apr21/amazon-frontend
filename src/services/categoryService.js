963e715be0b7ac392f4d41cf1ed342e703cf0a50
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-23 16:04:19 +0530

    Removed console.log

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -4,7 +4,6 @@ const baseUrl = 'http://localhost:3001/api/category';
 export const getAllCategories = async () => {
   try {
     const { data } = await axios.get(baseUrl);
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
