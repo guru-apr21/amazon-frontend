e67c3d9c2e9feed6d8e37b09e082988eb5f442aa
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-24 21:54:26 +0530

    Removed classNames and destructured multiple argument

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -2,16 +2,19 @@ import React, { forwardRef } from 'react';
 import ErrorMessage from './ErrorMessage';
 
 const Input = forwardRef(
-  ({ label, name, type = 'text', min, error, onBlur }, ref) => {
+  (
+    { label, name, min, error, onBlur, multiple = false, type = 'text' },
+    ref
+  ) => {
     return (
       <>
-        <label className="form__label">{label}</label>
+        <label>{label}</label>
         <input
-          className="form__input"
           name={name}
           ref={ref}
           type={type}
           onBlur={onBlur}
+          multiple={multiple}
         ></input>
         <ErrorMessage label={label} min={min} error={error}></ErrorMessage>
       </>
