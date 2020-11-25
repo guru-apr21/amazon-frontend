fb6d6268b05e60f4a992a52d04c16429fd16798c
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-25 15:37:37 +0530

    New button using styled component. Delete css

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import '../../css/Button.css';
+import styled from 'styled-components';
 
-function Button({ label, handler }) {
-  return (
-    <button className="button" onClick={handler}>
-      {label}
-    </button>
-  );
-}
+const Button = styled.button`
+  border-radius: 3px;
+  border: 1px solid;
+  line-height: 1.5;
+  margin: 0.6em 0;
+  border-color: ${({ secondary }) =>
+    secondary ? 'black' : '#a88734 #9c7e31 #846a29'};
+  background-color: ${({ secondary }) => (secondary ? '#f2f3f6' : '#f0c14b')};
+  padding: 0.3em 0.6em;
+  color: black;
+  width: max-content;
+  :not(:disabled) {
+    cursor: pointer;
+  }
+  ${({ $style }) => $style ?? {}}
+`;
 
 export default Button;
